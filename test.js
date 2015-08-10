18dae0105341b9acb46cf638114dfbe7e2064a35
Gennaro Del Sorbo <gennaro.delsorbo@gmail.com>
2015-08-10 12:39:55 +0000

    validate request

--- test.js
+++
@@ -1,7 +0,0 @@
-var cache = require("./lib/cache");
-
-cache.instantiate(600);
-cache.set("test", "test");
-cache.get("test").then(function(test){
-    console.log(test);
-});
--- test/indexTest.js
+++ test/indexTest.js
@@ -4,16 +4,16 @@ require("should");
 
 
 describe("Index", function(){
-    describe("Configuration", function(){
+    describe("Validate configuration", function(){
         it("if configuration not provided, throw exception", function(){
-            index.should.throw();
+            index.should.throw("Configuration not provided");
         });
         
         it("if ldap url not provided, throw exception", function() {
             (function(){
                 index({});
             }).should.throw("ldapUrl parameter not provided");
-        })
+        });
         
         it("if baseDN not provided, throw exception", function() {
             (function(){
@@ -21,7 +21,7 @@ describe("Index", function(){
                     ldapUrl: 'ldap://test.domain.com'
                 });
             }).should.throw("baseDN parameter not provided");
-        })
+        });
         
         it("if ldapUsername not provided, throw exception", function() {
             (function(){
@@ -30,7 +30,7 @@ describe("Index", function(){
                     baseDN: 'dc=domain,dc=com'
                 });
             }).should.throw("ldapUsername parameter not provided");
-        })
+        });
         
         it("if ldapPassword not provided, throw exception", function() {
             (function(){
@@ -40,7 +40,7 @@ describe("Index", function(){
                     ldapUsername: 'test'
                 });
             }).should.throw("ldapPassword parameter not provided");
-        })
+        });
         
         it("if ttl not provided, chose the default value", function(){
             var cache = require("../lib/cache");
@@ -54,4 +54,35 @@ describe("Index", function(){
             spy.calledWith(600).should.be.true();
         });
    }); 
+   
+   describe("Validate request", function() {
+       
+       it("if connection is undefined, throw exception", function(){
+       
+           var middleware = index({
+                ldapUrl: 'ldap://test.domain.com',
+                baseDN: 'dc=domain,dc=com',
+                ldapUsername: 'test',
+                ldapPassword: 'test'
+            });
+            
+            (function(){
+                middleware({});    
+            }).should.throw("The request does not contain NTLM info. Please install express-ntlm first and include it as express middleware");
+       });
+       
+       it("if ntlm is undefined, throw exception", function(){
+       
+           var middleware = index({
+                ldapUrl: 'ldap://test.domain.com',
+                baseDN: 'dc=domain,dc=com',
+                ldapUsername: 'test',
+                ldapPassword: 'test'
+            });
+            
+            (function(){
+                middleware({connection:{}});    
+            }).should.throw("The request does not contain NTLM info. Please install express-ntlm first and include it as express middleware");
+       });
+   });
 });
